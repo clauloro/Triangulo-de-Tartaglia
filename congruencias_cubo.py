02ff2317eba235250471e4c94ca5892b7ff48626
clauloro <114728388+clauloro@users.noreply.github.com>
2023-05-08 17:12:53 +0200

    triangulo de tartalia

--- congruencias_cubo.py
+++ congruencias_cubo.py
@@ -1,47 +1,34 @@
-def verificar_validez_configuracion(cubo):
-    # Paridad de las aristas
-    paridad_aristas = sum([cubo.aristas.index(i) % 2 for i in range(12)]) % 2
-    
-    # Paridad de las esquinas
-    paridad_esquinas = sum([cubo.esquinas.index(i) % 2 for i in range(8)]) % 2
-    
-    # Suma total de los giros de las aristas
-    giros_totales_aristas = sum([cubo.orientaciones_aristas[i] for i in range(12)]) % 2
-    
-    # Suma total de los giros de las esquinas
-    giros_totales_esquinas = sum([cubo.orientaciones_esquinas[i] for i in range(8)]) % 3
+import turtle
 
-    # Verificar si todas las condiciones de congruencia se cumplen
-    if paridad_aristas == 0 and paridad_esquinas == 0 and giros_totales_aristas == 0 and giros_totales_esquinas == 0:
-        return True
-    else:
-        return False
+def generate_tartaglia_triangle(n):
+    triangle = [[1]]
 
-# Ejemplo 1 devuelve True
-class CuboRubik:
-    def __init__(self):
-        self.aristas = list(range(12))
-        self.esquinas = list(range(8))
-        self.orientaciones_aristas = [0] * 12
-        self.orientaciones_esquinas = [0] * 8
+    for i in range(1, n):
+        row = [1]
 
-cubo_resuelto = CuboRubik()
-print(verificar_validez_configuracion(cubo_resuelto))
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
 
-#Ejemplo 2 devuelve False
-cubo_invalido = CuboRubik()
-# Cambiamos la orientación de la esquina 0
-cubo_invalido.orientaciones_esquinas[0] = 1
-print(verificar_validez_configuracion(cubo_invalido))  
+        row.append(1)
+        triangle.append(row)
 
-# Este código es solo un ejemplo de cómo se podría verificar la validez de una configuración del cubo de Rubik utilizando congruencias.
+    return triangle
 
-#Esta función, verificar_validez_configuracion, toma como entrada un objeto cubo que representa el cubo de Rubik 
-#con sus respectivas aristas, esquinas y orientaciones. La función verifica si la paridad de las aristas, la paridad 
-#de las esquinas, la suma de los giros de las aristas y la suma de los giros de las esquinas cumplen con las restricciones 
-#de congruencia. Si todas las condiciones se cumplen, la función devuelve True, indicando que la configuración del cubo es 
-#válida; de lo contrario, devuelve False.
+def draw_tartaglia_triangle(triangle):
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(-150, 150)
 
-#Este código que he proporcionado no es una solución completa para resolver el cubo de Rubik. Por lo general, al solucionar el cubo de Rubik, 
-#yo empleo métodos y técnicas más sofisticadas, como la teoría de grupos y algoritmos específicos para resolver el cubo. Además, mi código necesita 
-#que previamente haya creado una representación del cubo de Rubik en Python, lo cual no está incluido aquí.
+    for row in triangle:
+        x_start = turtle.xcor()
+        for cell_value in row:
+            turtle.write(cell_value, align="center", font=("Arial", 12, "normal"))
+            turtle.forward(30)
+        y_start = turtle.ycor() - 30
+        turtle.goto(x_start, y_start)
+
+    turtle.hideturtle()
+    turtle.done()
+
+tartaglia_triangle = generate_tartaglia_triangle(10)
+draw_tartaglia_triangle(tartaglia_triangle)
--- cubo_rubik.py
+++ cubo_rubik.py
@@ -1,131 +1,40 @@
-import pygame
-import random
+def generate_tartaglia_triangle(n):
+    triangle = [[1]]
 
-class RubikCube:
-    def __init__(self):
-        self.cube = [['w' for _ in range(2)] for _ in range(2)], \
-                    [['r' for _ in range(2)] for _ in range(2)], \
-                    [['b' for _ in range(2)] for _ in range(2)], \
-                    [['o' for _ in range(2)] for _ in range(2)]
+    for i in range(1, n):
+        row = [1]
 
-    def rotate_front(self, clockwise = True):
-        if clockwise:
-            temp = [self.cube[0][0][0], self.cube[0][0][1]]
-            self.cube[0][0][0] = self.cube[0][1][0]
-            self.cube[0][0][1] = self.cube[0][1][1]
-            self.cube[0][1][0] = temp[0]
-            self.cube[0][1][1] = temp[1]
-            # Rotate adjacent faces
-            temp = [self.cube[1][0][0], self.cube[1][1][0]]
-            self.cube[1][0][0] = self.cube[3][1][0]
-            self.cube[1][1][0] = self.cube[3][0][0]
-            self.cube[3][1][0] = self.cube[2][0][0]
-            self.cube[3][0][0] = self.cube[2][1][0]
-            self.cube[2][0][0] = temp[0]
-            self.cube[2][1][0] = temp[1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
 
-    def rotate_top(self, clockwise = True):
-        if clockwise:
-            temp = [self.cube[0][0][0], self.cube[0][0][1]]
-            self.cube[0][0][0] = self.cube[0][0][1]
-            self.cube[0][0][1] = self.cube[0][1][1]
-            self.cube[0][1][1] = self.cube[0][1][0]
-            self.cube[0][1][0] = temp[0]
-            # Rotate adjacent faces
-            temp = [self.cube[4][0][0], self.cube[4][0][1]]
-            self.cube[4][0][0] = self.cube[4][1][0]
-            self.cube[4][0][1] = self.cube[4][1][1]
-            self.cube[4][1][0] = temp[0]
-            self.cube[4][1][1] = temp[1]
-            temp = [self.cube[1][0][0], self.cube[1][0][1]]
-            self.cube[1][0][0] = self.cube[5][0][1]
-            self.cube[1][0][1] = self.cube[5][1][1]
-            self.cube[5][0][1] = self.cube[3][1][0]
-            self.cube[5][1][1] = self.cube[3][0][0]
-            self.cube[3][1][0] = temp[0]
-            self.cube[3][0][0] = temp[1]
+        row.append(1)
+        triangle.append(row)
 
-    def rotate_right(self, clockwise = True):
-        if clockwise:
-            temp = [self.cube[0][1][0], self.cube[0][1][1]]
-            self.cube[0][1][0] = self.cube[0][0][1]
-            self.cube[0][1][1] = self.cube[0][1][1]
-            self.cube[0][0][1] = self.cube[0][1][0]
-            self.cube[0][1][0] = temp[0]
-            # Rotate adjacent faces
-            temp = [self.cube[4][0][1], self.cube[4][1][1]]
-            self.cube[4][0][1] = self.cube[2][0][0]
-            self.cube[4][1][1] = self.cube[2][1][0]
-            self.cube[2][0][0] = self.cube[5][1][1]
-            self.cube[2][1][0] = self.cube[5][0][1]
-            self.cube[5][1][1] = temp[0]
-            self.cube[5][0][1] = temp[1]
-    
-    def rotate_back(self, clockwise=True):
-        if clockwise:
-            temp = [self.cube[2][0][0], self.cube[2][0][1]]
-            self.cube[2][0][0] = self.cube[2][1][0]
-            self.cube[2][0][1] = self.cube[2][1][1]
-            self.cube[2][1][0] = temp[0]
-            self.cube[2][1][1] = temp[1]
-            # Rotate adjacent faces
-            temp = [self.cube[1][1][0], self.cube[3][1][0]]
-            self.cube[1][1][0] = self.cube[4][1][0]
-            self.cube[3][1][0] = self.cube[5][1][0]
-            self.cube[4][1][0] = self.cube[1][1][0]
-            self.cube[5][1][0] = temp[1]
-    
-    def rotate_left(self, clockwise=True):
-        if clockwise:
-            temp = [self.cube[0][0][0], self.cube[0][1][0]]
-            self.cube[0][0][0] = self.cube[0][1][0]
-            self.cube[0][1][0] = self.cube[0][1][1]
-            self.cube[0][1][1] = self.cube[0][0][1]
-            self.cube[0][0][1] = temp[0]
-            # Rotate adjacent faces
-            temp = [self.cube[4][0][0], self.cube[4][1][0]]
-            self.cube[4][0][0] = self.cube[2][0][1]
-            self.cube[4][1][0] = self.cube[2][1][1]
-            self.cube[2][0][1] = self.cube[5][1][0]
-            self.cube[2][1][1] = self.cube[5][0][0]
-            self.cube[5][1][0] = temp[0]
-            self.cube[5][0][0] = temp[1]
-    
-    def rotate_down(self, clockwise=True):
-        if clockwise:
-            temp = [self.cube[2][0][0], self.cube[2][0][1]]
-            self.cube[2][0][0] = self.cube[2][0][1]
-            self.cube[2][0][1] = self.cube[2][1][1]
-            self.cube[2][1][1] = self.cube[2][1][0]
-            self.cube[2][1][0] = temp[0]
-            # Rotate adjacent faces
-            temp = [self.cube[1][0][1], self.cube[1][1][1]]
-            self.cube[1][0][1] = self.cube[4][0][1]
-            self.cube[1][1][1] = self.cube[4][1][1]
-            self.cube[4][0][1] = self.cube[3][1][1]
-            self.cube[4][1][1] = self.cube[3][0][1]
-            self.cube[3][1][1] = temp[0]
-            self.cube[3][0][1] = temp[1]
-            
-    def is_solved(self):
-        return all(self.cube[0][i][j] == 'w' for i in range(2) for j in range(2)) and \
-               all(self.cube[1][i][j] == 'r' for i in range(2) for j in range(2)) and \
-               all(self.cube[2][i][j] == 'b' for i in range(2) for j in range(2)) and \
-               all(self.cube[3][i][j] == 'o' for i in range(2) for j in range(2))
-
-    def shuffle(self):
-        for _ in range(20):
-            face = random.randint(0, 3)
-            direction = random.choice([True, False])
-            if face == 0:
-                self.rotate_front(direction)
-            elif face == 1:
-                self.rotate_top(direction)
+    return triangle
 
 
-#Este ejemplo de código utiliza Pygame para crear una interfaz gráfica para el usuario 
-# y permite que el usuario manipule el cubo mediante eventos de teclado o ratón.
-#La función "shuffle" desordena el cubo y la función "is_solved" determina si el cubo está resuelto.
-#La funcion draw es para dibujar el cubo en la interfaz gráfica.
+def binomial_coefficient(triangle, n, k):
+    if n < k:
+        raise ValueError("n must be greater or equal to k")
+
+    return triangle[n][k]
+
+
+def main():
+    # Generar el triángulo de Tartaglia de 10 filas
+    tartaglia_triangle = generate_tartaglia_triangle(10)
+
+    # Imprimir el triángulo de Tartaglia
+    for row in tartaglia_triangle:
+        print(row)
+
+    # Calcular e imprimir algunos coeficientes binomiales utilizando el triángulo de Tartaglia
+    print("C(5, 2) =", binomial_coefficient(tartaglia_triangle, 5, 2))
+    print("C(7, 3) =", binomial_coefficient(tartaglia_triangle, 7, 3))
+    print("C(9, 4) =", binomial_coefficient(tartaglia_triangle, 9, 4))
+
+
+if __name__ == "__main__":
+    main()
 
 
